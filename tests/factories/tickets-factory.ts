b3110bc9bfc8361424586823dd52dc32a7c3d459
Masih-Saldanha <masih.saldanha@gmail.com>
2022-08-31 14:19:57 -0300

    feat: add Tickets Tests

---
+++ tests/factories/tickets-factory.ts
@@ -0,0 +1,38 @@
+import { prisma } from "@/config";
+
+export async function createAvailableTicket() {
+//   const data: {
+//     presential: boolean;
+//     userId?: number;
+//   }[] = [
+//     { presential: true, userId: null },
+//     { presential: true, userId: null },
+//     { presential: true, userId: null },
+//     { presential: false, userId: null },
+//     { presential: false, userId: null },
+//     { presential: false, userId: null },
+//   ];
+
+//   const requisition = await prisma.ticket.createMany({
+//     data,
+//     skipDuplicates: true,
+//   });
+
+  const createPresential = await prisma.ticket.create({
+    data: {
+      presential: true,
+      userId: null,
+    },
+  });
+
+  const createOnline = await prisma.ticket.create({
+    data: {
+      presential: false,
+      userId: null,
+    },
+  });
+
+  return [createPresential, createOnline];
+
+//   return { requisition, data };
+}
--- tests/helpers.ts
+++ tests/helpers.ts
@@ -11,6 +11,7 @@ export async function cleanDb() {
   await prisma.event.deleteMany({});
   await prisma.session.deleteMany({});
   await prisma.user.deleteMany({});
+  await prisma.ticket.deleteMany({});
 }
 
 export async function generateValidToken(user?: User) {
